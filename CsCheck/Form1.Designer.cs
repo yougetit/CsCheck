313fb4ef8e73c0796ddc584ada6a3b3ca2f575d6
yougetit <yougetit@mail2000.com.tw>
2012-10-17 18:46:41 +0800

    正式版本V1.0發行，版本顯示於視窗上

--- CsCheck/Form1.Designer.cs
+++ CsCheck/Form1.Designer.cs
@@ -433,7 +433,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "健保卡查詢程式";
+            this.Text = "台中靜和醫院--健保卡查詢程式 V1.0";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.tabControl1.ResumeLayout(false);
